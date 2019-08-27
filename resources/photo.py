0b4c72d69c66f326959f7ec7ba894e11508c4080
David Zinser <dmzinser@gmail.com>
2019-08-26 23:03:05 -0700

    Final Commit

--- resources/photo.py
+++ resources/photo.py
@@ -37,31 +37,31 @@ def show_one_photo(id):
   print(photo.__dict__, "<== THIS IS THE PHOTO DICT IN THE SHOW ROUTE")
   return jsonify(data=model_to_dict(photo), status={"code": 200, "message": "Success"})
 
-def save_picture(form_picture):
-  random_hex = secrets.token_hex(8)
-  f_name, f_ext = os.path.splitext(form_picture.filename)
-  picture_name = random_hex + f_ext
-  file_path_for_photo = os.path.join(os.getcwd(), 'static/photo_uploads/' + picture_name)
+# def save_picture(form_picture):
+#   random_hex = secrets.token_hex(8)
+#   f_name, f_ext = os.path.splitext(form_picture.filename)
+#   picture_name = random_hex + f_ext
+#   file_path_for_photo = os.path.join(os.getcwd(), 'static/photo_uploads/' + picture_name)
   
-  output_size = (250, 250)
-  i = Image.open(form_picture)
-  i.thumbnail(output_size)
-  i.save(file_path_for_photo)
-  return picture_name
+#   output_size = (250, 250)
+#   i = Image.open(form_picture)
+#   i.thumbnail(output_size)
+#   i.save(file_path_for_photo)
+#   return picture_name
 
-@photo.route('/addphoto', methods=["POST"])
-def add_photo():
-  pay_file = request.files
-  payload = request.form.to_dict()
-  print(payload, '<== THIS IS THE PAYLOAD')
-  dict_file = pay_file.to_dict()
-  print(dict_file, '<== THIS IS THE ADD PHOTO PAYLOAD')
-  print(current_user.get_id(), '<== THIS IS THE CURRENT_USER')
-  print(pay_file, '<== THIS IS THE PAY_FILE')
-  file_picture_path = save_picture(dict_file['file_location'])
-  payload['file_location'] = file_picture_path
-  payload['user'] = id
-  photo = models.Photo.create(**payload)
-  photo_dict = model_to_dict(photo)
-  print(photo_dict, '<== THIS IS THE PHOTO_DICT')
-  return jsonify(data=photo_dict, status={"code": 201, "message": "Success"})
+# @photo.route('/addphoto', methods=["POST"])
+# def add_photo():
+#   pay_file = request.files
+#   payload = request.form.to_dict()
+#   print(payload, '<== THIS IS THE PAYLOAD')
+#   dict_file = pay_file.to_dict()
+#   print(dict_file, '<== THIS IS THE ADD PHOTO PAYLOAD')
+#   print(current_user.get_id(), '<== THIS IS THE CURRENT_USER')
+#   print(pay_file, '<== THIS IS THE PAY_FILE')
+#   file_picture_path = save_picture(dict_file['file_location'])
+#   payload['file_location'] = file_picture_path
+#   payload['user'] = id
+#   photo = models.Photo.create(**payload)
+#   photo_dict = model_to_dict(photo)
+#   print(photo_dict, '<== THIS IS THE PHOTO_DICT')
+#   return jsonify(data=photo_dict, status={"code": 201, "message": "Success"})
